1c5e7bae1b4881e1c3239e38b39cc4c5e8f3802d
Nico Fantaxis <nicospider2002@yahoo.com.ar>
2018-09-25 20:53:42 -0300

    Casi listo

--- src/TP1/Ejercicio_3.py
+++ src/TP1/Ejercicio_3.py
@@ -8,37 +8,56 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots(1, 1)
 
 def ajuste_extremos(x):
-    x = [i * (1-2*delta) for i in x]
+    x = [i * (1-2*delta) + delta for i in x]
     #return (1-2*delta)*x+delta
-    x = [i + delta for i in x]
+    #x = [i + delta for i in x]
     return x
 
-
-n=101 #cant de ptos de F(x) preferentemente impar para considerar el 0 
-delta=0.01 #valores maximos y minimos de la F(x)
-w=(1-2*delta)/(n-1) #ancho de bin
 semilla = (80560 + 85977) // 2  # type: int
+num = 18500 #cantidad de puntos a simular
+n=981 #cant de intervalos de F(x) preferentemente impar para considerar el 0 
+delta=0.01 #valores maximos y minimos de la F(x)
+w=(1-2*delta)/(n-1) #ancho de bin para la acumulada de la normal F(x)
+
+m = 101  #cant de intervalos de f(x) => intervalos del histograma
+gamma = 3.0 #valores maximos y minimos de la normal f(x)
+q=(2*gamma)/(m-1) #ancho de bin para la acumulada de la normal F(x)
 
 
-x = np.linspace(norm.ppf(delta),norm.ppf(1-delta), n)
-print x
+x = np.linspace(norm.ppf(delta),norm.ppf(1-delta), n) #invierte la normal
+max_x = max(x)
+min_x = min(x)
+#print x
 generador = GenNums.GeneradorNumeros()
-valores = generador.generar_numeros(semilla, 1000, True)
+valores = generador.generar_numeros(semilla, num, True)
 
 #valores = map((1-2*delta)*valores+delta, valores)
 res = ajuste_extremos(valores)
 #u=0.581 #ejemplo fijo
 #k=int(math.floor(u/w))-1 #indice de bin
 
-k = [int(math.floor(i / w))-1 for i in res]
 #inversa = ((u - (delta+k*w))/w) * (x[k+1]-x[k])+x[k]
 #print inversa
+max_res = max(res)
+min_res = min(res)
 
 inversas = []
 
-for index, value in enumerate(res):
-    inv = ((value - (delta+k[index]*w))/w) * (x[k[index]+1]-x[k[index]])+x[k[index]]
-    #inv = np.interp(value,[delta+k[index]*w, delta+(k[index]+1)*w],[x[k[index]],x[k[index]+1]])
+for value in res:
+    k=int(math.floor((value-delta) / w)) # Este es mi indice de bin
+    #interpola los valores de la F^-1(x)
+    inv = ((value - (delta+k*w))/w) * (x[k+1]-x[k])+x[k] 
+    
+    #inv = np.interp(value,[delta+k*w, delta+(k+1)*w],[x[k],x[k+1]])
     inversas.append(inv)
-print inversas
+#print inversas
 
+max_inv = max(inversas)
+min_inv = min(inversas)
+h = [0] * m
+for value in inversas:
+    k = int(math.floor((value+gamma)/q))
+    h[k] = h[k]+1
+print sum(h)
+#p, bins, patches = plt.hist(inversas, m , density=True, facecolor='g', alpha=0.75)
+plt.show(h)
--- src/TP1/Ejercicio_4.py
+++ src/TP1/Ejercicio_4.py
@@ -5,32 +5,28 @@ import GeneradorNumeros as GenNums
 
 from scipy.stats import norm
 import matplotlib.pyplot as plt
+from random import *
 
 
 
 fig, ax = plt.subplots(1, 1)
 delta = 0.01
-media_normal = 35.0
-desvio_e_normal = 5.0
-lamb = 1 / 35.0
+mu = 35.0
+sigma = 5.0
+#lamb = 1 / 35.0
+lamb = 1
 #Hay que Normalizar la Dist. Normal
-#z = x-media_normal/desvio_e_normal
-#x = z*desvio_e_normal+media_normal
-
-def ajuste_extremos(x):
-    x = [i * (1-2*delta) for i in x]
-    #return (1-2*delta)*x+delta
-    x = [i + delta for i in x]
-    return x
+#z = x-mu/sigma
+#x = z*sigma+mu
 
 def inversa_exponencial(x):
     return -math.log(1-x)/lamb
 
 def funcion_normal_estandard(x):
-    return (1/math.sqrt(2*math.pi*desvio_e_normal**2))*math.e**(-x**2/2)
+    return (1/math.sqrt(2*math.pi*sigma**2))*math.e**(-x**2/2)
 
 def funcion_normal(x):
-    return (1/math.sqrt(2*math.pi))*math.e**((-1*(x-media_normal)**2)/(2*desvio_e_normal**2))
+    return (1/math.sqrt(2*math.pi))*math.e**((-1*(x-mu)**2)/(2*sigma**2))
 
 def funcion_exponencial(x,lamb):
     return (lamb*math.e**(-lamb*x))
@@ -38,19 +34,33 @@ def funcion_exponencial(x,lamb):
 def funcion_exponencial_vs_normal_standard(x):
     return ((math.sqrt(2/math.pi))*math.e**(x-(x**2/2)))
 
-maximo = funcion_normal(media_normal)
+def desnormalizar(z,mu,sigma):
+    return [mu+i*sigma for i in z]
 
 semilla = (80560 + 85977) // 2  # type: int
+semilla2 = semilla+10000  # type: int
 generador = GenNums.GeneradorNumeros()
-valores = generador.generar_numeros(semilla, 1000, True)
-res = ajuste_extremos(valores)
+
+c = math.sqrt(2*math.e/math.pi) #cota normal estandar / exponencial
+num = 100 #cantidad de valores a simular
 
 
-for index, value in enumerate(valores):
-    pass
-    #lamb=1
-    probabilidad_aceptar = funcion_normal_estandard(value)/(maximo*funcion_exponencial(value,lamb))
-    if probabilidad_aceptar <  0.5:
-        pass
-    else:
-        pass
+z = []
+
+i=0
+while i<num:
+    t = random()
+    s = random()
+    p = funcion_normal_estandard(t)/(c*funcion_exponencial(t,lamb))
+    if s<p:
+        u = random()
+        if u < 0.5:
+            z.append(t)
+            i = i+1
+        else:
+            z.append(-t)
+            i = i+1
+
+#probabilidad_aceptar = funcion_normal_estandard(t)/(c*funcion_exponencial(t,lamb))
+x = desnormalizar(z,mu,sigma)
+print x
